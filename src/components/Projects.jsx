5780d039b7decd4b1078ba7ec362be4209947edc
AQz07 <christinaquirozcorrea@gmail.com>
2025-05-15 13:44:23 -0400

    add bullet point extra assistant

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -10,7 +10,7 @@ function Projects() {
       <h2 className='text-4xl font-bold text-center text-color-6 mb-16'>Mis proyectos</h2>
       <div className='bg-color-6 rounded-lg p-8 lg:p-14 mb-10'>
           <h3 className='text-white text-2xl font-bold text-center mb-3'>Asistente Conversacional implementado con Búsqueda Híbrida <span className='text-blue-400'> Graph RAG</span></h3>
-          <h5 className='text-center text-blue-400 font-semibold mb-8'>MongoDB &bull; ExpressJS &bull; ReactJS &bull; NodeJS &bull; Python &bull; Neo4j &bull; LangChain</h5>
+          <h5 className='text-center text-blue-400 font-semibold mb-8'>MongoDB &bull; ExpressJS &bull; ReactJS &bull; NodeJS &bull; Tailwind CSS &bull; Python &bull; Neo4j &bull; LangChain</h5>
           <ul className="list-disc list-inside marker:text-blue-500 text-white space-y-2 text-left lg:mx-20">
             <li className='text-justify'>
               Proyecto de Grado para el Museo Catedralicio de la Catedral Metropolitana de Santa Cruz, 
